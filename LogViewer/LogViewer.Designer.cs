2691cd2e87a3d02c565bfa9d90c0894c79b8e0fa
JyqPro <297912959@qq.com>
2025-09-03 11:31:42 +0800

    commit project

---
+++ LogViewer/LogViewer.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LogViewer
+{
+    partial class LogViewer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogViewer));
+            toolStrip1 = new ToolStrip();
+            statusStrip1 = new StatusStrip();
+            splitContainer1 = new SplitContainer();
+            tree_testList = new TreeView();
+            imageList1 = new ImageList(components);
+            txt_content = new RichTextBox();
+            btn_open = new ToolStripButton();
+            btn_loadLog = new ToolStripButton();
+            btn_close = new ToolStripButton();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btn_open, btn_loadLog, btn_close });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1328, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Location = new Point(0, 748);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1328, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 27);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(tree_testList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(txt_content);
+            splitContainer1.Size = new Size(1328, 721);
+            splitContainer1.SplitterDistance = 442;
+            splitContainer1.TabIndex = 2;
+            // 
+            // tree_testList
+            // 
+            tree_testList.Dock = DockStyle.Fill;
+            tree_testList.ImageIndex = 0;
+            tree_testList.ImageList = imageList1;
+            tree_testList.Location = new Point(0, 0);
+            tree_testList.Name = "tree_testList";
+            tree_testList.SelectedImageIndex = 0;
+            tree_testList.Size = new Size(442, 721);
+            tree_testList.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "file16.png");
+            // 
+            // txt_content
+            // 
+            txt_content.Dock = DockStyle.Fill;
+            txt_content.Location = new Point(0, 0);
+            txt_content.Name = "txt_content";
+            txt_content.ReadOnly = true;
+            txt_content.Size = new Size(882, 721);
+            txt_content.TabIndex = 0;
+            txt_content.Text = "";
+            // 
+            // btn_open
+            // 
+            btn_open.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btn_open.Image = Resource.open_folder32;
+            btn_open.ImageTransparentColor = Color.Magenta;
+            btn_open.Name = "btn_open";
+            btn_open.Size = new Size(29, 24);
+            btn_open.Text = "打开日志";
+            // 
+            // btn_loadLog
+            // 
+            btn_loadLog.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btn_loadLog.Image = Resource.folder32;
+            btn_loadLog.ImageTransparentColor = Color.Magenta;
+            btn_loadLog.Name = "btn_loadLog";
+            btn_loadLog.Size = new Size(29, 24);
+            btn_loadLog.Text = "仅加载日志";
+            // 
+            // btn_close
+            // 
+            btn_close.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btn_close.Image = Resource.close32;
+            btn_close.ImageTransparentColor = Color.Magenta;
+            btn_close.Name = "btn_close";
+            btn_close.Size = new Size(29, 24);
+            btn_close.Text = "退出";
+            // 
+            // LogViewer
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1328, 770);
+            Controls.Add(splitContainer1);
+            Controls.Add(statusStrip1);
+            Controls.Add(toolStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "LogViewer";
+            Text = "LogViewer";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private StatusStrip statusStrip1;
+        private SplitContainer splitContainer1;
+        private TreeView tree_testList;
+        private RichTextBox txt_content;
+        private ImageList imageList1;
+        private ToolStripButton btn_open;
+        private ToolStripButton btn_loadLog;
+        private ToolStripButton btn_close;
+    }
+}
---
+++ LogViewer/LogViewer.cs
@@ -0,0 +1,164 @@
+namespace LogViewer
+{
+    public partial class LogViewer : Form
+    {
+        private const string INFO = "[INFO ]";
+        private const string TEST_ITEM_START_INFO = "[INFO ] LUA: ============================== 开始测试 ";
+        private const string MARK_CHAR = "=";
+        public LogViewer()
+        {
+            InitializeComponent();
+
+            StartPosition = FormStartPosition.CenterScreen;
+            btn_open.Click -= Btn_open_Click;
+            btn_open.Click += Btn_open_Click;
+
+            tree_testList.AfterSelect -= Tree_testList_AfterSelect;
+            tree_testList.AfterSelect += Tree_testList_AfterSelect;
+            btn_close.Click -= Btn_close_Click;
+            btn_close.Click += Btn_close_Click;
+            btn_loadLog.Click -= Btn_loadLog_Click;
+            btn_loadLog.Click += Btn_loadLog_Click;
+
+            tree_testList.DragEnter -= Tree_testList_DragEnter;
+            tree_testList.DragDrop -= Tree_testList_DragDrop;
+            txt_content.DragEnter -= Txt_content_DragEnter;
+            txt_content.DragDrop -= Txt_content_DragDrop;
+            txt_content.AllowDrop = true;
+            tree_testList.AllowDrop = true;
+            tree_testList.DragEnter += Tree_testList_DragEnter;
+            tree_testList.DragDrop += Tree_testList_DragDrop;
+            txt_content.DragEnter += Txt_content_DragEnter;
+            txt_content.DragDrop += Txt_content_DragDrop;
+        }
+
+        private void Txt_content_DragDrop(object sender, DragEventArgs e)
+        {
+            var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (paths is null || paths.Length <= 0) return;
+            ReadFile(paths[0], false);
+        }
+
+        private void Txt_content_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Tree_testList_DragDrop(object sender, DragEventArgs e)
+        {
+            var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (paths is null || paths.Length <= 0) return;
+            ReadFile(paths[0]);
+        }
+
+        private void Tree_testList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Btn_loadLog_Click(object sender, EventArgs e)
+        {
+            ReadFile(loadTestItems: false);
+        }
+
+        private void Btn_close_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Tree_testList_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node is null)
+            {
+                return;
+            }
+            HighlightAllMatches(txt_content, e.Node.Text);
+        }
+
+        private void Btn_open_Click(object sender, EventArgs e)
+        {
+            ReadFile();
+        }
+        private void ReadFile(string file = "", bool loadTestItems = true)
+        {
+            try
+            {
+                var fileDialog = new OpenFileDialog
+                {
+                    Filter = "log文件|*.log|txt文件|*.txt",
+                    Title = "选择日志文件"
+                };
+                if (string.IsNullOrEmpty(file))
+                {
+                    if (fileDialog.ShowDialog() != DialogResult.OK)
+                    {
+                        return;
+                    }
+                }
+
+                string searchText = TEST_ITEM_START_INFO;
+                string filePath = string.IsNullOrEmpty(file) ? fileDialog.FileName : file;
+                var lines = File.ReadAllLines(filePath);
+                var logLines = lines.Select((line, index) => line).Where(x => x.Contains(searchText));
+                txt_content.Clear();
+                txt_content.Lines = lines;
+                if (loadTestItems)
+                {
+                    tree_testList.Nodes.Clear();
+                    foreach (var item in logLines)
+                    {
+                        var name = GetTestItemName(item);
+                        var node = new TreeNode(name, 0, 0);
+                        tree_testList.Nodes.Add(node);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"读取文件出错：{ex.Message}");
+            }
+        }
+        private static string GetTestItemName(string line)
+        {
+            int infoIndex = line.IndexOf(INFO);
+            var target = line[infoIndex..].Replace(TEST_ITEM_START_INFO, "");
+            var index = target.IndexOf(MARK_CHAR);
+            var result = target[..(index - 1)];
+            return result;
+        }
+        private static void HighlightAllMatches(RichTextBox richTextBox, string searchText)
+        {
+            richTextBox.SelectAll();
+            richTextBox.SelectionBackColor = richTextBox.BackColor;
+            richTextBox.SelectionColor = richTextBox.ForeColor;
+
+            int index = richTextBox.Find(searchText, 0, RichTextBoxFinds.None);
+
+            if (index < 0)
+            {
+                return;
+            }
+            richTextBox.Select(index, searchText.Length);
+            richTextBox.ScrollToCaret();
+
+            richTextBox.SelectionBackColor = Color.Yellow;
+            richTextBox.SelectionColor = Color.Black;
+
+            richTextBox.Focus();
+        }
+    }
+}
